f332d2b35a191bb1cdebaa78c66f5f396fd2a934
Matt <mhalstead0@gmail.com>
2018-03-12 22:53:31 -0400

    Added daily usage calculation to OutputFileWriter

--- cmep-generator/src/main/java/com/matthalstead/cmepgenerator/OutputFileWriter.java
+++ cmep-generator/src/main/java/com/matthalstead/cmepgenerator/OutputFileWriter.java
@@ -1,7 +1,9 @@
 package com.matthalstead.cmepgenerator;
 
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Random;
 
 /**
  * 
@@ -12,14 +14,50 @@ public class OutputFileWriter {
 
 	public static void printOutput(InputRecord inputRecord, PrintWriter pw, LocalDate usageDate) {
 		LocalDate lastReadingDate = inputRecord.getLastReadingDate();
-		int dayStepSize;
-		if (usageDate.isBefore(lastReadingDate)) {
-			dayStepSize = -1;
+		boolean goingForward = usageDate.isAfter(lastReadingDate);
+		BigDecimal startReading = null;
+		BigDecimal endReading = null;
+		if (goingForward) {
+			endReading = inputRecord.getLastReading();
+			LocalDate date = lastReadingDate.plusDays(1);
+			while (!date.isAfter(usageDate)) {
+				startReading = endReading;
+				BigDecimal dayUsage = getDailyUsage(inputRecord, date);
+				endReading = startReading.add(dayUsage);
+				date = date.plusDays(1);
+			}
 		} else {
-			dayStepSize = 1;
+			endReading = inputRecord.getLastReading();
+			LocalDate date = lastReadingDate;
+			while (!date.isBefore(usageDate)) {
+				BigDecimal dayUsage = getDailyUsage(inputRecord, date);
+				endReading = endReading.subtract(dayUsage);
+				date = date.minusDays(1);
+			}
+			startReading = endReading.subtract(getDailyUsage(inputRecord, usageDate));
 		}
+		pw.println(startReading + "-" + endReading);
+		
 		//TODO finish
 		
+	}
+	
+	private static BigDecimal getDailyUsage(InputRecord inputRecord, LocalDate date) {
+		int hash = date.hashCode();
+		if (inputRecord.getPremiseNumber() != null) {
+			hash ^= inputRecord.getPremiseNumber().hashCode();
+		}
+		Random r = new Random(hash);
+		double adu = inputRecord.getAverageDailyUsage().doubleValue();
+		double stdev = adu / 4.0;
+		if (stdev < 0.5) {
+			stdev = 0.5;
+		}
+		double x = (r.nextDouble() * stdev) + adu;
+		if (x < 0.0) {
+			x = 0.0;
+		}
+		return new BigDecimal(x).setScale(Utils.USAGE_SCALE, Utils.ROUNDING_MODE);
 	}
 	
 }
--- cmep-generator/src/main/java/com/matthalstead/cmepgenerator/Utils.java
+++ cmep-generator/src/main/java/com/matthalstead/cmepgenerator/Utils.java
@@ -1,6 +1,7 @@
 package com.matthalstead.cmepgenerator;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 public abstract class Utils {
@@ -8,9 +9,13 @@ public abstract class Utils {
 	
 	public static final int USAGE_SCALE = 3;
 	
+	public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_EVEN;
+	
+	public static final MathContext MATH_CONTEXT = new MathContext(USAGE_SCALE, ROUNDING_MODE);
+	
 	public static BigDecimal parseUsageValue(String str) {
 		BigDecimal bd = new BigDecimal(str);
-		BigDecimal result = bd.setScale(USAGE_SCALE, RoundingMode.HALF_EVEN);
+		BigDecimal result = bd.setScale(USAGE_SCALE, ROUNDING_MODE);
 		return result;
 	}
 }
